6452d6ac7c4b0878217f98398bd00194c9e81d58
Chris Moore <57775710+chrism-48@users.noreply.github.com>
2022-06-01 17:36:09 -0400

    added styles to cards

--- quiz/src/App.vue
+++ quiz/src/App.vue
@@ -28,6 +28,12 @@ export default {
       description: 'Sample description'
       },{title: 'Chapter 2',
       description: 'Sample description two'
+      },{title: 'Chapter 3',
+      description: 'Sample description three'
+      },{title: 'Chapter 4',
+      description: 'Sample description four'
+      },{title: 'Chapter 5',
+      description: 'Sample description five'
       }]
       
       
@@ -43,4 +49,13 @@ export default {
 #app {  
   margin-top: 60px;
 }
+.ui.centered.card {
+  border-style:solid;
+  border-width:.01px;
+  border-color:black;
+  box-shadow: 2px 2px 10px rgba(0, 0, 0, 0.502);
+}
+.ui.centered.card:hover {
+  background-color:rgba(0, 0, 0, 0.253);
+} 
 </style>
